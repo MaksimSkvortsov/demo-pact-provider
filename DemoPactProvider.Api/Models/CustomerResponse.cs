8dc59161111c3ae920f538e64cea4ca5ae87d57d
Max <maksimskvortsov@gmail.com>
2026-09-06 12:04:28 -0400

    Remove name from customer response

--- DemoPactProvider.Api/Models/CustomerResponse.cs
+++ DemoPactProvider.Api/Models/CustomerResponse.cs
@@ -2,6 +2,5 @@ namespace DemoPactProvider.Api.Models;
 
 public sealed record CustomerResponse(
     int Id,
-    string Name,
     string Email,
     string Phone);
--- DemoPactProvider.Api/Program.cs
+++ DemoPactProvider.Api/Program.cs
@@ -6,7 +6,11 @@ public partial class Program
 {
     public static WebApplication BuildApp(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
+        var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+        {
+            Args = args,
+            ApplicationName = typeof(Program).Assembly.GetName().Name
+        });
 
         builder.Services.AddControllers();
 
